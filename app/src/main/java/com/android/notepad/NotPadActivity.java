deb7dd725c9915a93c286fa910d7c6aaed776d67
DESKTOP-2GALT9J\jhs <hyunsu.dev@gmail.com>
2021-04-30 10:38:07 +0900

    메모장 notepad db생성(DBHelper.java) 기본적인 crud 구현

--- app/src/main/java/com/android/notepad/NotPadActivity.java
+++ app/src/main/java/com/android/notepad/NotPadActivity.java
@@ -2,12 +2,15 @@ package com.android.notepad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import com.android.R;
 
 public class NotPadActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
